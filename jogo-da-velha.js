0dd039bd6f8b23621801fa8a862f9bf8de077f7f
Paulo Lima <102117492+Pucapuka@users.noreply.github.com>
2024-01-25 12:50:50 -0300

    Update jogo-da-velha.js
    script do jogo

--- jogo-da-velha.js
+++ jogo-da-velha.js
@@ -1,6 +1,136 @@
-var vez = "X"
-function change(){
-    let button = document.getElementById('a1-button');
+var vez = "O"
+
+function change_a1(){
+    let button = document.getElementById('button-a1');
+  	if (vez==="O"){
+  	    button.innerHTML="O";
+  	    button.style.color = "red";
+  	    vez = "X";
+  	    button.disabled="true";
+  	}else{
+  	    button.innerHTML="X";
+  	    button.style.color = "green";
+  	    vez="O";
+  	    button.disabled="true";
+  	}
+  	
+}
+
+function change_a2(){
+    let button = document.getElementById('button-a2');
+  	if (vez==="O"){
+  	    button.innerHTML="O";
+  	    button.style.color = "red";
+  	    vez = "X";
+  	    button.disabled="true";
+  	}else{
+  	    button.innerHTML="X";
+  	    button.style.color = "green";
+  	    vez="O";
+  	    button.disabled="true";
+  	}
+  	
+}
+
+function change_a3(){
+    let button = document.getElementById('button-a3');
+  	if (vez==="O"){
+  	    button.innerHTML="O";
+  	    button.style.color = "red";
+  	    vez = "X";
+  	    button.disabled="true";
+  	}else{
+  	    button.innerHTML="X";
+  	    button.style.color = "green";
+  	    vez="O";
+  	    button.disabled="true";
+  	}
+  	
+}
+
+function change_b1(){
+    let button = document.getElementById('button-b1');
+  	if (vez==="O"){
+  	    button.innerHTML="O";
+  	    button.style.color = "red";
+  	    vez = "X";
+  	    button.disabled="true";
+  	}else{
+  	    button.innerHTML="X";
+  	    button.style.color = "green";
+  	    vez="O";
+  	    button.disabled="true";
+  	}
+  	
+}
+
+function change_b2(){
+    let button = document.getElementById('button-b2');
+  	if (vez==="O"){
+  	    button.innerHTML="O";
+  	    button.style.color = "red";
+  	    vez = "X";
+  	    button.disabled="true";
+  	}else{
+  	    button.innerHTML="X";
+  	    button.style.color = "green";
+  	    vez="O";
+  	    button.disabled="true";
+  	}
+  	
+}
+
+function change_b3(){
+    let button = document.getElementById('button-b3');
+  	if (vez==="O"){
+  	    button.innerHTML="O";
+  	    button.style.color = "red";
+  	    vez = "X";
+  	    button.disabled="true";
+  	}else{
+  	    button.innerHTML="X";
+  	    button.style.color = "green";
+  	    vez="O";
+  	    button.disabled="true";
+  	}
+  	
+}
+
+function change_c1(){
+    let button = document.getElementById('button-c1');
+  	if (vez==="O"){
+  	    button.innerHTML="O";
+  	    button.style.color = "red";
+  	    vez = "X";
+  	    button.disabled="true";
+  	}else{
+  	    button.innerHTML="X";
+  	    button.style.color = "green";
+  	    vez="O";
+  	    button.d
+  	    isabled="true";
+  	}
+  	
+}
+
+function change_c2(){
+    let button = document.getElementById('button-c2');
+  	if (vez==="O"){
+  	    button.innerHTML="O";
+  	    button.style.color = "red";
+  	    vez = "X";
+  	    button.disabled="true";
+  	}else{
+  	    button.innerHTML="X";
+  	    button.style.color = "green";
+  	    vez="O";
+  	    button.disabled="true";
+  	}
+  	
+}
+
+function change_c3(){
+    let button = document.getElementById('button-c3');
   	if (vez==="O"){
   	    button.innerHTML="O";
   	    button.style.color = "red";
